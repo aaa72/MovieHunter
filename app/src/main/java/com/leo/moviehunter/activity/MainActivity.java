e4f64aded219ab843fa6af2e1a31957656cc2b7a
Leo <aaa72kimo@gmail.com>
2017-02-23 01:23:28 +0800

    Import ToolBar environment

--- app/src/main/java/com/leo/moviehunter/activity/MainActivity.java
+++ app/src/main/java/com/leo/moviehunter/activity/MainActivity.java
@@ -1,15 +1,19 @@
 package com.leo.moviehunter.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.leo.moviehunter.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
